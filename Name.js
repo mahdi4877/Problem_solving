46232762bfc2e56b0f9b97288a67eb440c710e5b
mahdi hasan <mahdihasan4877@gmail.com>
2026-08-13 18:38:28 +0600

    Add name problem

---
+++ Name.js
@@ -0,0 +1,7 @@
+// Using multiple console.log statements:
+console.log("Mahdi");
+console.log("Computer Science");
+console.log("My University Name");
+
+// Alternatively, using a single statement with newline characters:
+console.log("Mahdi\nComputer Science\nMy University Name");
