47a811e44da275c54a2fe5af13597f1b39cf5708
mahdi hasan <mahdihasan4877@gmail.com>
2026-08-13 18:24:15 +0600

    Add VariablesandTemplate problem

---
+++ VariablesandTemplate.js
@@ -0,0 +1,7 @@
+const name = "Alex";
+const department = "Computer Science";
+let semester = 3;
+
+console.log(
+  `My name is ${name}. I am in the ${department} department, currently in semester ${semester}.`,
+);
