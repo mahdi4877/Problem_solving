fa561f9a61b64c270d281a3aab92b0c41c684880
mahdi hasan <mahdihasan4877@gmail.com>
2026-08-20 22:00:38 +0600

    add fizzbuzz problem

--- pyramid.js
+++ pyramid.js
@@ -1,11 +1,13 @@
-let rows = 5;
+for (let i = 1; i <= 5; i++) {
+  let row = "";
 
-for (let i = 1; i <= rows; i++) {
-  let line = "";
-
-  for (let j = 1; j <= i; j++) {
-    line += "* ";
+  for (let j = 1; j <= 5 - i; j++) {
+    row += " ";
   }
 
-  console.log(line);
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    row += "*";
+  }
+
+  console.log(row);
 }
