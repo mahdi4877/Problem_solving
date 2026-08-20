bf5e6aee7a0b385c578f06bd016c504f18df339f
mahdi hasan <mahdihasan4877@gmail.com>
2026-08-20 21:48:03 +0600

    add season problem

---
+++ season.js
@@ -0,0 +1,36 @@
+let month = 7;
+
+switch (month) {
+  case 1:
+  case 2:
+    console.log("Winter");
+    break;
+
+  case 3:
+  case 4:
+    console.log("Spring");
+    break;
+
+  case 5:
+  case 6:
+    console.log("Summer");
+    break;
+
+  case 7:
+  case 8:
+    console.log("Monsoon");
+    break;
+
+  case 9:
+  case 10:
+    console.log("Autumn");
+    break;
+
+  case 11:
+  case 12:
+    console.log("Late Autumn");
+    break;
+
+  default:
+    console.log("Invalid month");
+}
