e2fbaf5eab5d53dcc57dd899f87122528cd454ee
mahdi hasan <mahdihasan4877@gmail.com>
2026-08-21 21:39:01 +0600

    add this problem

---
+++ create_bank_ac.js
@@ -0,0 +1,16 @@
+function createBankAccount(initial) {
+  let balance = initial;
+
+  return {
+    deposit(amount) {
+      balance += amount;
+    },
+    getBalance() {
+      return balance;
+    },
+  };
+}
+
+const account = createBankAccount(100);
+account.deposit(50);
+console.log(account.getBalance());
