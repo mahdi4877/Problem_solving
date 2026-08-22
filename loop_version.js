91dc9ce5fc5614d0d5f726b1af5bedabec4aacb8
mahdi hasan <mahdihasan4877@gmail.com>
2026-08-22 19:16:22 +0600

    add this in github

---
+++ loop_version.js
@@ -0,0 +1,12 @@
+function findMaxLoop(arr) {
+  if (arr.length === 0) return null;
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+console.log(findMaxLoop([3, 7, 2, 9, 4]));
